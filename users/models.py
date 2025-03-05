25acaa246ab9efa0d677991338665f7831bfcb2d
mobatra <mohammedalbattrawi@gmail.com>
2025-03-05 14:48:51 +0200

    solve the conflict of the abstract user with custom user in groupe and user permissions [related_name = custom_user_""_set

--- users/models.py
+++ users/models.py
@@ -1,13 +1,19 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class User(AbstractUser):
-  ROLES_CHOICES = [
-    ('admin','Admin'),
-    ('user', 'User')
-  ]
-  email = models.EmailField(unique=True)
-  role = models.CharField(max_length=10 , choices= ROLES_CHOICES , default='user')
-  def __str__(self):
-    return f"{self.username} | {self.role}"
-  
+    email = models.EmailField(unique=True)
+    
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="custom_user_set",  
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="custom_user_permissions_set",
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
