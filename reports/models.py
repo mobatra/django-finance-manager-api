f416214d25fbe42d96df98a4e1b009cd5e946dbb
mobatra <mohammedalbattrawi@gmail.com>
2025-03-05 06:12:49 +0200

    create reports model and repair for s3 url to store file in database

--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Report(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reports")
+    generated_at = models.DateTimeField(auto_now_add=True)
+    file_url = models.URLField()
+
+    def __str__(self):
+        return f"Report for {self.user.username} - {self.generated_at}"
+  
