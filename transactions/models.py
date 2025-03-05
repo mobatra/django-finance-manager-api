31682f258926d62b4dc1fc5c290ef8ae18c9abf3
mobatra <mohammedalbattrawi@gmail.com>
2025-03-05 06:11:24 +0200

    create transaction model

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from .choices import CATEGORY_CHOICES
 
-# Create your models here.
+User = get_user_model()
+
+class Transaction(models.Model):
+      user = models.ForeignKey(User, on_delete= models.CASCADE, related_name="transaction")
+      amount = models.DecimalField(max_digits=10, decimal_places=2)
+      category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+      description = models.TextField(blank=True, null=True)
+      date = models.DateField(auto_now_add=True)
+      def __str__(self):
+        return f"{self.user.username} | {self.category} | {self.amount}"
