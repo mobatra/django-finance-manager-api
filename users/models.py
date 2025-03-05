e574e76ffbacd8623fe5580514173a366e46254e
mobatra <mohammedalbattrawi@gmail.com>
2025-03-05 06:09:41 +0200

    create user model using abstract user class to make custom user model and override on email

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+  ROLES_CHOICES = [
+    ('admin','Admin'),
+    ('user', 'User')
+  ]
+  email = models.EmailField(unique=True)
+  role = models.CharField(max_length=10 , choices= ROLES_CHOICES , default='user')
+  def __str__(self):
+    return f"{self.username} | {self.role}"
+  
