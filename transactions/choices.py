b182fd712c3c591c3ceadfdd8df0b1a3ee3be900
mobatra <mohammedalbattrawi@gmail.com>
2025-03-05 06:10:56 +0200

    create choices file to store all categories of the budgets and transactions , this file work as centralized

---
+++ transactions/choices.py
@@ -0,0 +1,10 @@
+# Centralized category choices
+CATEGORY_CHOICES = [
+    ('food', 'Food'),
+    ('transport', 'Transport'),
+    ('rent', 'Rent'),
+    ('shopping', 'Shopping'),
+    ('utilities', 'Utilities'),
+    ('entertainment', 'Entertainment'),
+    ('others', 'Others'),
+]
