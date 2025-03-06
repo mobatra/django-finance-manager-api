c0f19de171556830b0ee9f5348a4ee8369e1c2dd
mobatra <mohammedalbattrawi@gmail.com>
2025-03-06 13:05:14 +0200

    create user serializer

---
+++ users/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.Serializer):
+    class Meta:
+      model = User 
+      fields = ['id', 'username', 'email', 'password']
+      
+      def create(self, validated_data):
+        username = validated_data['username']
+        email = validated_data['email']
+        password = validated_data['password']
+        user = User.objects.create_user(username, email, password)
+        return user
+      
