279ac13704737547932cca51672ba1e1905e53dc
mobatra <mohammedalbattrawi@gmail.com>
2025-03-08 15:54:36 +0200

    configure login route and token , refresh token

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import UserViewSet
 
 router = DefaultRouter()
@@ -7,4 +8,6 @@ router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
