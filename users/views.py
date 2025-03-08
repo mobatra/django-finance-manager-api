e6438042219fa9ae35b3111535ce1b77c0ebe1ee
mobatra <mohammedalbattrawi@gmail.com>
2025-03-08 21:05:37 +0200

    handle the logout view api

--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
 from django.contrib.auth import get_user_model
 from .serializers import UserSerializer
-from rest_framework.permissions import AllowAny, IsAuthenticated
 
 User = get_user_model()
 print(User)
@@ -14,3 +18,14 @@ class UserViewSet(ModelViewSet):
             return [AllowAny()]
         else:
             return [IsAuthenticated()]
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self,request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"detail": "Logout successful."}, status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response({"error": "Invalid token."}, status=status.HTTP_400_BAD_REQUEST)            
