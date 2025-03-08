daf1f8285dff9a4bfd8b575e869f99dc425321d4
mobatra <mohammedalbattrawi@gmail.com>
2025-03-08 21:05:54 +0200

    configure the logout rout

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import UserViewSet
+from .views import UserViewSet, LogoutView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
@@ -9,5 +9,6 @@ router.register(r'users', UserViewSet, basename='user')
 urlpatterns = [
     path('', include(router.urls)),
     path('login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('logout', LogoutView.as_view(), name='logout'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
