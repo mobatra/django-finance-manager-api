9563e78187b3255eb8a48de09a5d436286e8f0a5
mobatra <mohammedalbattrawi@gmail.com>
2025-03-08 15:53:01 +0200

    fix: put the create function outside the meta class

--- users/serializers.py
+++ users/serializers.py
@@ -9,9 +9,9 @@ class UserSerializer(serializers.ModelSerializer):
       model = User 
       fields = ['id', 'username', 'email', 'password']
       
-      def create(self, validated_data):
-        username = validated_data['username']
-        email = validated_data['email']
-        password = validated_data['password']
-        user = User.objects.create_user(username, email, password)
-        return user
+    def create(self, validated_data):
+      username = validated_data['username']
+      email = validated_data['email']
+      password = validated_data['password']
+      user = User.objects.create_user(username, email, password)
+      return user
