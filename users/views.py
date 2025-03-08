8f596d9b8ffaa9760066b9213105fdc454545a9c
mobatra <mohammedalbattrawi@gmail.com>
2025-03-08 05:56:05 +0200

    create users view to handle crud users api

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+User = get_user_model()
+
+class UserViewSet(ModelViewSet):
+    query_set = User.objects.all()
+    serializer_class = UserSerializer
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]
+        else:
+            return [IsAuthenticated()]
