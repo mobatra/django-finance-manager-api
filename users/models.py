2a57c8b09c8a59cc4c9c31ee6a42f2e8edeeb0c1
mobatra <mohammedalbattrawi@gmail.com>
2025-03-12 16:07:40 +0200

    handle listener to check permission

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
@@ -17,3 +19,8 @@ class User(AbstractUser):
     
     def __str__(self):
         return self.username
+@receiver(post_save, sender=User)
+def assign_default_group(sender, instance, created, **kwargs):
+    if created:
+        user_group, _ = Group.objects.get_or_create(name="User")
+        instance.groups.add(user_group)    
