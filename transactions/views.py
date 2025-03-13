cde6ea3fe80c0d4c3fe34db88146e0ecee2bde09
mobatra <mohammedalbattrawi@gmail.com>
2025-03-13 09:22:20 +0200

    handle transactions view

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from .models import Transaction
+from .serializers import TransactionSerializer
 
-# Create your views here.
+class TransactionListCreateView(ListCreateAPIView):
+  serializer_class = TransactionSerializer
+  permission_classes = [permissions.IsAuthenticated]
+  def get_queryset(self):
+    return Transaction.objects.filter(user=self.request.user)
+  
+  def perform_create(self, serializer):
+    serializer.save(user=self.request.user)
+    
+  def create(self, request, *args, **kwargs):
+    response = super().create(request, *args, **kwargs)
+    return Response(
+        {
+          "message": "Transaction created successfully",
+          "data": response.data
+        },
+        status=status.HTTP_201_CREATED
+        )
+  
+class TransactionDetailView(RetrieveUpdateDestroyAPIView):
+  serializer_class = TransactionSerializer
+  permission_classes = [permissions.IsAuthenticated]
+  def get_queryset(self):
+    return Transaction.objects.filter(user=self.request.user)
