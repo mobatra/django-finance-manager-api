0d734bd939506c3bc46a623fbe9c39787cd49648
mobatra <mohammedalbattrawi@gmail.com>
2025-03-13 12:35:15 +0200

    configure the pagination and filtering

--- finance_manager/settings.py
+++ finance_manager/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    'django_filters',
     'users',
     'transactions',
     'budgets',
@@ -37,6 +38,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE':5,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 
