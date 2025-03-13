69117d40b103bae756cabf58fad61b2a7555cf4c
mobatra <mohammedalbattrawi@gmail.com>
2025-03-13 12:36:10 +0200

    refactor: edit the transaction api

--- finance_manager/urls.py
+++ finance_manager/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('users.urls')),
-    path('api/v1/transactions', include('transactions.urls'))
+    path('api/v1/transactions/', include('transactions.urls'))
 ]
