57cbd7c8e75b4d78c052d14edcdf4ff7c25a0252
mobatra <mohammedalbattrawi@gmail.com>
2025-03-16 13:42:26 +0200

    configure the url of the s3 upload api

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TransactionListCreateView, TransactionDetailView
+from .views import TransactionListCreateView, TransactionDetailView, GenerateUploadURL,GenerateDownloadURL
 
 urlpatterns = [
     path("", TransactionListCreateView.as_view(), name="transaction-list-create"),
-    path("/<int:pk>", TransactionDetailView.as_view(), name="transaction-detail")
+    path("<int:pk>", TransactionDetailView.as_view(), name="transaction-detail"),
+    path("s3/upload-url/", GenerateUploadURL.as_view(), name="generate-upload-url"),
+    path("s3/download-url/<str:filename>/", GenerateDownloadURL.as_view(), name="generate-download-url"),
 ]
