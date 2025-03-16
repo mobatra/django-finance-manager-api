8bfd9029931b2d4c06b49d13d572f5bb33ddb965
mobatra <mohammedalbattrawi@gmail.com>
2025-03-16 13:39:44 +0200

    refactor code

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -3,13 +3,12 @@ from rest_framework import serializers
 from .models import Transaction
 
 class TransactionSerializer(ModelSerializer):
-  class Meta:
-    model = Transaction
-    fields = ["id", "user", "amount", "category", "description", "date"]
-    read_only_fields = ["id", "user", "date"]
-  def validate_amount(self,value):
-    if value <= 0:
-      raise serializers.ValidationError("Amount must be greater than zero.")
-    return value
+    class Meta:
+        model = Transaction
+        fields = ["id", "user", "amount", "category", "description", "date", "receipt"]
+        read_only_fields = ["id", "user", "date"]
 
-  
+    def validate_amount(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Amount must be greater than zero.")
+        return value
