06e752759db9a0f2bc4d3bb724133785f218a79f
mobatra <mohammedalbattrawi@gmail.com>
2025-03-16 13:35:25 +0200

    modify the the receipts field to store url form the s3 bucket

--- transactions/models.py
+++ transactions/models.py
@@ -10,5 +10,6 @@ class Transaction(models.Model):
       category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
       description = models.TextField(blank=True, null=True)
       date = models.DateField(auto_now_add=True)
+      receipt = models.URLField(blank=True, null=True)
       def __str__(self):
         return f"{self.user.username} | {self.category} | {self.amount}"
